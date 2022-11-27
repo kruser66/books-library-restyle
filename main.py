c8fb5e55eef9c5bfd09a4a3f053de6355aca1a2c
kruser <kruser@yandex.ru>
2022-11-27 21:20:39 +0500

    download books with titles

--- main.py
+++ main.py
@@ -2,24 +2,12 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
+from collections import namedtuple
 
 
 def check_for_redirect(response):
     if response.history:
         raise requests.HTTPError
-
-
-def download_book(books_dir, book_id):
-    url = f'https://tululu.org/txt.php?id={book_id}'
-
-    response = requests.get(url)
-    response.raise_for_status()
-
-    check_for_redirect(response)
-
-    path_to_save = os.path.join(books_dir, f'id{book_id}.txt')
-    with open(path_to_save, 'wb') as file:
-        file.write(response.content)
 
 
 def download_txt(url, filename, folder='books/'):
@@ -35,7 +23,7 @@ def download_txt(url, filename, folder='books/'):
     response = requests.get(url)
     response.raise_for_status()
 
-    # check_for_redirect(response)
+    check_for_redirect(response)
 
     path_to_save = os.path.join(folder, sanitize_filename(filename) + '.txt')
 
@@ -45,19 +33,11 @@ def download_txt(url, filename, folder='books/'):
     return path_to_save
 
 
-def main():
-    books_dir = 'books'
-    os.makedirs(books_dir, exist_ok=True)
-
-    for book_id in range(1, 11):
-        try:
-            download_book(books_dir, book_id)
-        except requests.HTTPError:
-            print(f'Book with id {book_id} - not exist')
-
-
 def parse_page_book(book_id):
-    url = f'https://tululu.org/b{str(book_id)}/'
+
+    site = 'https://tululu.org/'
+
+    url = site + f'b{str(book_id)}/'
 
     response = requests.get(url)
     response.raise_for_status()
@@ -65,29 +45,29 @@ def parse_page_book(book_id):
     check_for_redirect(response)
 
     soup = BeautifulSoup(response.text, 'lxml')
-
     title_tag = soup.find('h1')
-
     title, author = title_tag.text.split('::')
 
-    print(f'Заголовок: {title.strip()}')
-    print(f'Автор: {author.strip()}')
+    Book = namedtuple('Book', 'title author url filename')
+    book = Book(
+        title.strip(),
+        author.strip(),
+        site + f'txt.php?id={book_id}',
+        f'{book_id}. {title.strip()}'
+    )
 
-    # print(image_tag['src'])
-    # print()
-    # print(text_block.text)
+    return book
+
+
+def main():
+
+    for book_id in range(1, 11):
+        try:
+            book = parse_page_book(book_id)
+            print(download_txt(book.url, book.filename))
+        except requests.HTTPError:
+            print(f'Book with id {book_id} - not exist')
 
 
 if __name__ == '__main__':
-    # main()
-    # parse_page_book(1)
-    url = 'http://tululu.org/txt.php?id=1'
-
-    filepath = download_txt(url, 'Алиби')
-    print(filepath)  # Выведется books/Алиби.txt
-
-    filepath = download_txt(url, 'Али/би', folder='books/')
-    print(filepath)  # Выведется books/Алиби.txt
-
-    filepath = download_txt(url, 'Али\\би', folder='txt/')
-    print(filepath)  # Выведется txt/Алиби.txt
+    main()
