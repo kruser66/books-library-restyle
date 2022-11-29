e2fc734d2ab387a56eab4b2cdcd6032f29a83a30
kruser <kruser@yandex.ru>
2022-11-29 15:40:41 +0500

    fixed review comments

--- tululu.py
+++ tululu.py
@@ -3,22 +3,22 @@ import re
 import sys
 import requests
 import argparse
+from time import sleep
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from collections import namedtuple
 from urllib.parse import urljoin, urlparse, unquote
 
 
-site = 'https://tululu.org/'
-
-
 def check_for_redirect(response):
 
-    if response.url == site or response.history:
+    if response.history:
         raise requests.HTTPError
 
 
 def fetch_book_page(book_id):
+
+    site = 'https://tululu.org/'
 
     url = urljoin(site, f'b{str(book_id)}/')
 
@@ -83,11 +83,11 @@ def parse_book_page(response):
     title, author = title_tag.text.split('::')
 
     txt_tag = soup.find(href=re.compile("/txt."))
-    txt_href = txt_tag['href'] if txt_tag else ''
-    txt_url = urljoin(site, txt_href)
+    txt_href = txt_tag['href'] if txt_tag else '/txt.php'
+    txt_url = urljoin(response.url, txt_href)
 
     image_tag = soup.find('div', class_='bookimage').find('img')
-    image_url = urljoin(site, image_tag['src'])
+    image_url = urljoin(response.url, image_tag['src'])
 
     tag_comments = soup.find_all('div', class_='texts')
     comments = '\n'.join([tag.find('span').text for tag in tag_comments])
@@ -133,26 +133,55 @@ def create_parser():
 
 def main():
 
+    connect_wait = 10
+
     parser = create_parser()
     if len(sys.argv) < 3:
         parser.print_help()
     args = parser.parse_args()
 
     for book_id in range(args.start_id, args.end_id + 1):
-        try:
-            response = fetch_book_page(book_id)
-            book = parse_book_page(response)
+        connection = True
 
-            cover_name = unquote(urlparse(book.image_url).path.split('/')[-1])
-            txt_name = f'{book_id}. {book.title}'
+        while True:
+            try:
+                response = fetch_book_page(book_id)
+                book = parse_book_page(response)
 
-            print(download_txt(book.txt_url, txt_name))
-            print(download_cover(book.image_url, cover_name))
-            print()
-        except requests.HTTPError:
-            print(
-                f'Книга с номером id: {book_id} не доступна для скачивания.\n'
-            )
+                cover_name = unquote(
+                    urlparse(book.image_url).path.split('/')[-1])
+                txt_name = f'{book_id}. {book.title}'
+
+                print(download_txt(book.txt_url, txt_name))
+                print(download_cover(book.image_url, cover_name))
+                print()
+                break
+            except requests.ConnectionError:
+                if connection:
+                    connection = False
+                    print(
+                        f'\nОтсутствует соединение с Итернет!'
+                        f'Повторная попытка соединения...'
+                    )
+                else:
+                    print('\nОтсутствует соединение с Итернет!')
+                    print(
+                        f'Повторная попытка соединения через'
+                        f' {connect_wait} секунд.'
+                    )
+                    sleep(connect_wait)
+            except requests.HTTPError:
+                print(
+                    f'Книга с номером id: {book_id} ',
+                    f'не доступна для скачивания.\n'
+                )
+                break
+            except Exception as error:
+                print(f'\nНепредвиденная ошибка: {error}')
+                print(
+                    f'\nКнига с номером id: {book_id} ',
+                    f'Проверьте! Возможна ошибка при загрузке.\n'
+                )
 
 
 if __name__ == '__main__':
